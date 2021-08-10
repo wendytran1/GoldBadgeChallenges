a7a72612f66c1feb108f1534f4cdf30bae62c789
Wendy Tran <wendytran321@gmail.com>
2021-08-10 15:01:48 -0400

    commit gold bagde challenges

---
+++ 01_Cafe_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("01_Cafe_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("01_Cafe_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8a1feaeb-8260-4b47-8a48-b44993a47714")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 01_Cafe_UI/Menu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Cafe_repo
+{
+    public class Menu
+    {
+        //this method Run starts the program
+        public void Run()
+        {
+            Menu();       //inside the method Run above, call Menu
+        }
+
+        public void MenuItems()     //first method MenuItems contains all items in the "menu"
+        {
+            bool keepRunning = true;
+            while (keepRunning) ;     //while keepRunning loop is true ...
+            {
+
+                //a menu with a list of 5 items to choose from
+                Console.WriteLine("Select a menu option:\n" +
+                    "1. Meal number\n" +
+                    "2. Meal name\n" +
+                    "3. Description\n" +
+                    "4. List of ingredients\n" +
+                    "5. Price\n");
+
+                string input = Console.ReadLine();      //whatever the user types will be read by Console.ReadLine, returned and saved as input string
+
+
+            }
+
+        }
+    }
+
+    public class MenuRepository
+    {
+
+    }
+
+    public class Test
+    {
+
+    }
+}
